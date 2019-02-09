8c1b3647460cedc5e00d7f27837b5bc720c3f141
Alex.Putrya <alex.putrya@gmail.com>
2019-02-09 19:37:56 +0200

    initial project

---
+++ app/Http/Controllers/Forms/NewRepair.php
@@ -0,0 +1,14 @@
+namespace App\Http\Controllers\Forms;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class NewRepair exyends Controller {
+    public function __invoke(Request $request){
+
+        //Принимаем данные в случае метода post
+        if($request->isMethod('post'){
+
+        }
+    }
+}
