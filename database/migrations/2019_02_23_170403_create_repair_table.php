c39532ade97b3a15774372ff30bab9b84f20066c
Alex.Putrya <alex.putrya@gmail.com>
2019-02-23 20:18:31 +0200

    Crate repair migration

---
+++ database/migrations/2019_02_23_170403_create_repair_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepairTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('users')) {
+            Schema::create('repair', function (Blueprint $table) {
+                $table->increments('id');
+                $table->date('register_date');
+                $table->date('end_date')->nullable();
+                $table->string('status');
+                $table->string('client');
+                $table->string('phone');
+                $table->string('device');
+                $table->string('serial')->nullable();
+                $table->string('complect')->nullable();
+                $table->string('defect')->nullable();
+                $table->foreign('owner')->references('id')->on('users')->onDelete('cascade');
+                
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+        Schema::dropIfExists('repair');
+    }
+}
